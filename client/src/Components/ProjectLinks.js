1e5b7d4554348da9325a404cd058eed8044a263f
Dex Fitch <dexter.fitch@smoothstack.com>
2024-03-07 19:54:47 -0500

    Favicon and links

--- client/src/Components/ProjectLinks.js
+++ client/src/Components/ProjectLinks.js
@@ -21,11 +21,11 @@ function ProjectLinks() {
       <h3 className="sub-title">Project Links</h3>
       {linkFactory()}
       <h3 className="sub-title pt-5">Social Media</h3>
-      &diams; <a href="https://www.instagram.com/erik.ransom/" target="_blank">Instagram</a><br />
-      &diams; <a href="https://twitter.com/ErikRansom" target="_blank">Twitter</a><br />
-      &diams; <a href="https://www.facebook.com/ErikJRansom/" target="_blank">Facebook</a><br />
-      &diams; <a href="https://www.linkedin.com/in/erik-ransom-97905940/" target="_blank">LinkedIn</a><br />
-      &diams; <a href="mailto:erikjransom@gmail.com" target="_blank">Email</a>
+      &diams; <a href="https://www.instagram.com/erik.ransom/" target="_blank" rel="noreferrer">Instagram</a><br />
+      &diams; <a href="https://twitter.com/ErikRansom" target="_blank" rel="noreferrer">Twitter</a><br />
+      &diams; <a href="https://www.facebook.com/ErikJRansom/" target="_blank" rel="noreferrer">Facebook</a><br />
+      &diams; <a href="https://www.linkedin.com/in/erik-ransom-97905940/" target="_blank" rel="noreferrer">LinkedIn</a><br />
+      &diams; <a href="mailto:erikjransom@gmail.com" target="_blank" rel="noreferrer">Email</a>
       <Buffer />
     </div>
   );
